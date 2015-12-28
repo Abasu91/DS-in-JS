645faec0f147f8be6ee4c120777255e6a0f37434
Abasu91 <arijitbasu948@gmail.com>
2015-12-28 13:12:17 -0600

    minor fix
    initializing the empty array directly as an argument

--- datas.js
+++ datas.js
@@ -3,97 +3,97 @@
  state of stack operations are maintained
 
  @author:Arijt Basu
- Log: Sun Dec 27, 2015, 3:25PM
+ Log: Sun Dec 27, 2016, 3:25PM
  ------------------------------------------------------------------- 
  */
-var stackControl = true ;
-var stack = (function(array){
-	array = [];
-	//--Define the max size of the stack
-	var MAX_SIZE = 5;
-	function isEmpty(){if(array.length<1) console.log("Stack is empty");};
-	isEmpty();
+var stackControl = true;
+var stack = (function(array) {
+        //array = [];
+        //--Define the max size of the stack
+        var MAX_SIZE = 5;
 
-	return {
-		
-		push: function(ele){
-			if(array.length<MAX_SIZE){
-				array.push(ele)
-				return array;
-		}
-		else{
-			console.log("Stack Overflow")
-			}
-		},
-		pop: function(){
-			if(array.length>1){
-		    	array.pop();
-		    	return array;
-			}
-		else{
-			console.log("Stack Underflow");
-		}	
-		}
+        function isEmpty() {
+            if (array.length < 1) console.log("Stack is empty");
+        };
+        isEmpty();
 
-	}
-})()
-// var list = 5;
-// console.log(stack(list))
-if(stackControl){
-console.log(stack.pop());
-console.log(stack.push(3));
-console.log(stack.push(2));
-console.log(stack.pop());
-console.log(stack.push(1));
-console.log(stack.pop());
-console.log(stack.push(38));
-console.log(stack.push(22));
-console.log(stack.pop());
-console.log(stack.pop());
-console.log(stack.push(6));
-console.log(stack.pop());
+        return {
+
+            push: function(ele) {
+                if (array.length < MAX_SIZE) {
+                    array.push(ele)
+                    return array;
+                } else {
+                    console.log("Stack Overflow")
+                }
+            },
+            pop: function() {
+                if (array.length > 1) {
+                    array.pop();
+                    return array;
+                } else {
+                    console.log("Stack Underflow");
+                }
+            }
+
+        }
+    })([])
+    // var list = 5;
+    // console.log(stack(list))
+if (stackControl) {
+    console.log(stack.pop());
+    console.log(stack.push(3));
+    console.log(stack.push(2));
+    console.log(stack.pop());
+    console.log(stack.push(1));
+    console.log(stack.pop());
+    console.log(stack.push(38));
+    console.log(stack.push(22));
+    console.log(stack.pop());
+    console.log(stack.pop());
+    console.log(stack.push(6));
+    console.log(stack.pop());
 }
 //End of STACK Logic
 
 /* Defining Queue operations*/
 
-var queue = (function(array){
-	array = [];
-	var reversearray;
-	//--Define the max size of the stack
-	var MAX_SIZE = 5;
-	function isEmpty(){if(array.length<1) console.log("Queue is empty");};
-	isEmpty();
+var queue = (function(array) {
+    //array = [];
+    var reversearray;
+    //--Define the max size of the stack
+    var MAX_SIZE = 5;
 
-	return {
-			insert: function(ele){
-			if(array.length<MAX_SIZE){
-				array.push(ele)
-				reversearray = array.reverse();
-				return reversearray;
-		}
-		else{
-			console.log("Queue Overflow")
-			}
-		},
-			delete: function(){
-				if(array.length>1){
-				    //reversearray = array.reverse();
-			    	array.pop();
-			    	return array;
-				}
-			else{
-				console.log("Queue Underflow");
-			}	
-		}
-	}
+    function isEmpty() {
+        if (array.length < 1) console.log("Queue is empty");
+    };
+    isEmpty();
+
+    return {
+        insert: function(ele) {
+            if (array.length < MAX_SIZE) {
+                array.push(ele)
+                reversearray = array.reverse();
+                return reversearray;
+            } else {
+                console.log("Queue Overflow")
+            }
+        },
+        delete: function() {
+            if (array.length > 1) {
+                //reversearray = array.reverse();
+                array.pop();
+                return array;
+            } else {
+                console.log("Queue Underflow");
+            }
+        }
+    }
 
 
 
-})()
+})([])
 
 console.log(queue.insert(5))
 console.log(queue.insert(3))
 console.log(queue.delete(3))
-
-
